579f09fbd77e144981e17a5d1bd722f0bcfb81ea
Belendor <61137829+Belendor@users.noreply.github.com>
2022-12-21 17:18:53 +0200

    init

--- backend.tf
+++ backend.tf
@@ -1,10 +1,4 @@
 terraform {
   backend "s3" {
-    bucket         = "alex-ilja-artur-tfstate-bucket"
-    key            = "workspaces/terraform.tfstate"
-    region         = "eu-central-1"
-
-    dynamodb_table = "alex-ilja-artur-tfstate-locks"
-    encrypt        = true
   }
 }
---
+++ env/backend.hcl
@@ -0,0 +1,5 @@
+bucket         = "alex-ilja-artur-tfstate-bucket"
+key            = "workspaces/terraform.tfstate"
+region         = "eu-central-1"
+dynamodb_table = "alex-ilja-artur-tfstate-locks"
+encrypt        = true
